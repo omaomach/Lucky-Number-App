fd1ba00b2bb4fd174a8c89d7d3214fe8f755f780
omaomach <aaronjoash44@gmail.com>
2023-06-22 14:26:13 +0300

    successfully gotten user data from first activity and passed it to the second

--- app/src/main/java/com/omao/luckynumber/LuckyNumberActivity.kt
+++ app/src/main/java/com/omao/luckynumber/LuckyNumberActivity.kt
@@ -2,10 +2,29 @@ package com.omao.luckynumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class LuckyNumberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lucky_number)
+
+        val tv2: TextView = findViewById(R.id.tv2)
+        val luckNumText: TextView = findViewById(R.id.luckNumText)
+        val shareBtn: Button = findViewById(R.id.shareBtn)
+
+        var userName = receiveUserName()
+        Toast.makeText(this, ""+userName, Toast.LENGTH_LONG).show()
+
     }
+
+    fun receiveUserName(): String {
+        var bundle: Bundle? = intent.extras
+        var userName = bundle?.get("name").toString()
+        return userName
+    }
+
 }
+
